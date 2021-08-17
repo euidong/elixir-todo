0c1e6857e93bcddd948f8d5deeafac11a23455bc
euidong <justicedong@naver.com>
2021-08-17 22:32:30 +0900

    Add: Registry and apply supervisor tree

--- test/todo_cache_test.exs
+++ test/todo_cache_test.exs
@@ -1,8 +1,12 @@
 defmodule TodoCacheTest do
   use ExUnit.Case
 
+  setup_all do
+    {:ok, todo_system_pid} = Todo.System.start_link()
+    {:ok, todo_system_pid: todo_system_pid}
+  end
+
   test "server_process" do
-    Todo.System.start_link()
     bob_pid = Todo.Cache.server_process("bob")
 
     assert bob_pid != Todo.Cache.server_process("alice")
@@ -10,7 +14,6 @@ defmodule TodoCacheTest do
   end
 
   test "to-do operations" do
-    Todo.System.start_link()
     jane = Todo.Cache.server_process("jane")
     Todo.Server.add_entry(jane, %{date: ~D[2018-12-19], title: "Dentist"})
     entries = Todo.Server.entries(jane, ~D[2018-12-19])
@@ -19,14 +22,11 @@ defmodule TodoCacheTest do
   end
 
   test "persistence" do
-    {:ok, supervisor} = Todo.System.start_link()
-
     john = Todo.Cache.server_process("john")
     Todo.Server.add_entry(john, %{date: ~D[2018-12-20], title: "Shopping"})
     assert 1 == length(Todo.Server.entries(john, ~D[2018-12-20]))
 
-    Supervisor.stop(supervisor)
-    Todo.System.start_link()
+    Process.exit(john, :kill)
 
     entries =
       "john"
